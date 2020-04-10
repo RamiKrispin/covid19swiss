fda12c3e3d5db8c45d90a40cb183172b81ccf37e
RamiKrispin <ramkrisp@umich.edu>
2020-04-10 07:15:08 -0700

    Update the data structure

--- data_raw/pull_data.R
+++ data_raw/pull_data.R
@@ -6,13 +6,13 @@
 `%>%` <- magrittr::`%>%`
 #-------------- Github list of files --------------
 # Required jq
-command <- 'curl "https://api.github.com/repos/openZH/covid_19/contents/fallzahlen_kanton_total_csv?ref=master" | jq ".[].name"'
+command <- 'curl "https://api.github.com/repos/openZH/covid_19/contents/fallzahlen_kanton_total_csv_v2?ref=master" | jq ".[].name"'
 
 files_list <- system(command = command, intern = TRUE)
 #-------------- Pulling the raw data --------------
 df_raw <- lapply(files_list, function(i){
   print(files_list)
-  df <- read.csv(paste("https://raw.githubusercontent.com/openZH/covid_19/master/fallzahlen_kanton_total_csv/", gsub('"', '', i), sep = ""))
+  df <- read.csv(paste("https://raw.githubusercontent.com/openZH/covid_19/master/fallzahlen_kanton_total_csv_v2/", gsub('"', '', i), sep = ""))
   return(df)
 }) %>% dplyr::bind_rows()
 #-------------- Cleaning the data --------------
@@ -24,9 +24,10 @@ covid19swiss <- df_raw %>%
   dplyr::group_by(date, canton) %>%
   dplyr::summarise(total_tested = sum(ncumul_tested, na.rm = TRUE),
                    total_confirmed = sum(ncumul_conf, na.rm = TRUE),
-                   total_hospitalise = sum(ncumul_hosp, na.rm = TRUE),
-                   total_ICU = sum(ncumul_ICU, na.rm = TRUE),
-                   total_vent = sum(ncumul_vent, na.rm = TRUE),
+                   new_hosp = sum(new_hosp, na.rm = TRUE),
+                   current_hosp = sum(current_hosp, na.rm = TRUE),
+                   current_icu = sum(current_icu, na.rm = TRUE),
+                   current_vent = sum(current_vent, na.rm = TRUE),
                    total_recovered = sum(ncumul_released, na.rm = TRUE),
                    total_death = sum(ncumul_deceased, na.rm = TRUE)) %>%
   tidyr::pivot_longer(c(-date, - canton),
